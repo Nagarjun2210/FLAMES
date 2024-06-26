8298de1c3c3d52c923422a38442cb5ae94d73d72
Nagarjun2210 <arjun79043@gmail.com>
2024-06-26 17:42:26 +0530

    first commit

---
+++ my-react-app/src/layout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from "react";
+import { useRef } from "react";
+import find_final_letter from './logic.js';
+
+function Layout(){
+    var [name_1, sn1] = useState("");
+    var [name_2, sn2] = useState("");
+    var output = useRef("");
+    function setOutput(name_1, name_2) {
+        output.current = find_final_letter(name_1, name_2);
+        document.getElementById("output").innerText = output.current;
+    };
+    
+    const layout = (
+        <div>
+            <h2>FLAMES</h2>
+                <label>Enter name 1
+                    <input id="name_1" type="text" onChange={(e) => sn1(e.target.value)} />
+                </label>
+                <label>Enter name 2
+                    <input id="name_2" type="text" onChange={(e) => sn2(e.target.value)} />
+                </label>
+                <button type="button" onClick={() => setOutput(name_1, name_2)}> Submit</button>
+            <p id="output"></p>
+        </div>
+    );
+return layout;
+};
+
+export default Layout;
